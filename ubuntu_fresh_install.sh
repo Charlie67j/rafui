ff4a8b441fa2a14438f0343212ee99690d1e0d77
Charlie67j <chendrixson@gmail.com>
2021-02-17 13:47:43 -0500

    Update ubuntu_fresh_install.sh

--- ubuntu_fresh_install.sh
+++ ubuntu_fresh_install.sh
@@ -84,7 +84,6 @@ Do you want to install docker? If so type y / If you don't wish to install enter
 read $docker
 
 if [[ $docker -eq "y" ]] || [[ $docker -eq "yes" ]]; then
-    sudo apt-get remove docker docker-engine docker.io containerd runc
     sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
     sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
