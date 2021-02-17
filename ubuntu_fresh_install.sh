318d28c1c39c7ef9949f4c04b21ea4272afdf084
Charlie67j <chendrixson@gmail.com>
2021-02-17 13:22:49 -0500

    Update ubuntu_fresh_install.sh

--- ubuntu_fresh_install.sh
+++ ubuntu_fresh_install.sh
@@ -4,7 +4,7 @@
 # This is a basic install, easily configurable to your needs
 #
 # Test to see if user is running with root privileges.
-if [[ "${UID}" -ne 0 ]]
+if [[ "${UID}" -ne 0 ]];
 then
  echo 'Must execute with sudo or root' >&2
  exit 1
